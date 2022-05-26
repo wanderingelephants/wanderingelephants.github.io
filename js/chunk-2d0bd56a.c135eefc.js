6d1d7a0e0a7cab9e46f3815e325c2b956dd4f8cc
wanderingelephants <sachet.singh@gmail.com>
2022-05-26 16:57:53 +0530

    updated

---
+++ js/chunk-2d0bd56a.c135eefc.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0bd56a"],{"2c25":function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("Coming soon!")])},o=[],u=t("2877"),l={},s=Object(u["a"])(l,c,o,!1,null,null,null);e["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0bd56a.c135eefc.js.map
